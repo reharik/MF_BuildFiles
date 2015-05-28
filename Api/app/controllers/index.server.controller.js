76c50d99efea143e6246471b8acd326c5700f532
reharik <reharik@gmail.com>
2015-05-27 21:54:46 -0500

    git ignore reapplied

--- Api/app/controllers/index.server.controller.js
+++
@@ -1,8 +0,0 @@
-var buildInfo = require('../../../build-info.json');
-
-exports.index = function *() {
-  this.body = yield this.render("basic", {
-    version: buildInfo.version,
-    commit: buildInfo.commit
-  });
-};
